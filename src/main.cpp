10f79110113a9f8c112d8685d01a4478833f6120
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2013-08-08 17:58:16 +0200

    Create main window of the application

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,20 @@
 /*
- *  Copyright (C) 2013  Riccardo Bellini <riccardo.bellini1988@gmail.com>
- * 
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- * 
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- * 
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
+    Copyright (C) 2013  Riccardo Bellini <riccardo.bellini1988@gmail.com>
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+   
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+   
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 #include <kaboutdata.h>
 #include <klocalizedstring.h>
@@ -22,6 +22,8 @@
 #include <kuniqueapplication.h>
 #include <kapplication.h>
 #include <kdebug.h>
+
+#include "mainwindow.h"
 
 
 static const char *version = "0.1";
@@ -46,7 +48,8 @@ int main(int argc, char **argv)
     
     KUniqueApplication app;
     
-    // TODO
+    MainWindow *w = new MainWindow;
+    w->show();
     
     return app.exec();
 }
