f55f18a9f5feb0be6fc353bd7ec519db7d2fb9d8
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2013-08-13 13:31:57 +0200

    Create database class for the application, include it as a library

---
+++ src/db/vrdatabase.cpp
@@ -0,0 +1,35 @@
+/*
+    Copyright (C) 2013  Riccardo Bellini <riccardo.bellini1988@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+// KDE includes
+#include <kstandarddirs.h>
+
+// Qt includes
+
+
+// Vehicle Reminder includes
+#include "vrdatabase.h"
+
+
+VRDatabase::VRDatabase(QObject *parent) : QObject(parent)
+{
+    QString type = "QSQLITE";
+    m_database = QSqlDatabase::addDatabase(type);
+    QString path = KStandardDirs::locateLocal("appdata", "vr.db");
+    m_database.setDatabaseName(path);
+}
---
+++ src/db/vrdatabase.h
@@ -0,0 +1,38 @@
+/*
+    Copyright (C) 2013  Riccardo Bellini <riccardo.bellini1988@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+#ifndef VRDATABASE_H
+#define VRDATABASE_H
+
+#include <qobject.h>
+#include <qsqldatabase.h>
+
+
+class QSqlDatabase;
+
+class VRDatabase : public QObject
+{
+public:
+    explicit VRDatabase(QObject *parent = 0);
+    
+private:
+    QSqlDatabase m_database;
+};
+
+#endif // VRDATABASE_H
