481eda02005e04c3311a454c4630a55d3b8bb96e
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2013-09-30 16:09:09 +0200

    Add class ProfileProxyModel
    ProfileProxyModel inherits from QAbstractProxyModel, and should provide
    a layer of abstraction between the ProfileView class and the
    QSqlTableModel of the "profile" table

---
+++ src/profileproxymodel.cpp
@@ -0,0 +1,88 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2013  Riccardo Bellini <riccardo.belini1988@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+// Vehicle Reminder includes
+#include "profileproxymodel.h"
+
+
+ProfileProxyModel::ProfileProxyModel(QObject * parent) : QAbstractProxyModel(parent)
+{
+
+}
+
+
+QModelIndex ProfileProxyModel::mapFromSource(const QModelIndex & sourceIndex)
+{
+    QModelIndex index;
+    
+    // TODO
+    
+    return index;
+}
+
+
+QModelIndex ProfileProxyModel::mapToSource(const QModelIndex & proxyIndex)
+{
+    QModelIndex index;
+    
+    // TODO
+    
+    return index;
+}
+
+
+int ProfileProxyModel::columnCount(const QModelIndex & parent)
+{
+    int result;
+    
+    // TODO
+    
+    return result;
+}
+
+
+int ProfileProxyModel::rowCount(const QModelIndex & parent)
+{
+    int result;
+    
+    // TODO
+    
+    return result;
+}
+
+
+QModelIndex ProfileProxyModel::parent(const QModelIndex & child)
+{
+    QModelIndex index;
+    
+    // TODO
+    
+    return index;
+}
+
+
+QModelIndex ProfileProxyModel::index(int row, int column, const QModelIndex & parent)
+{
+    QModelIndex index;
+    
+    // TODO
+    
+    return index;
+}
